da6e294adbbfd799f067b6375d2f20143f9fea2f
Uki Mahfuda <mahfuda948@gmail.com>
2024-12-02 11:28:25 +0700

    commit

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,24 @@
 FROM python:3.10-slim
 
 # Set variabel lingkungan untuk unbuffered logs
-ENV PYTHONUNBUFFERED True
+ENV PYTHONUNBUFFERED=True
+ENV PORT=8080  # Port default untuk Cloud Run
 
 # Set direktori kerja aplikasi
-ENV APP_HOME /app
+ENV APP_HOME=/app
 WORKDIR $APP_HOME
 
+# Install dependencies sistem untuk Pillow dan TensorFlow
+RUN apt-get update && apt-get install -y \
+    libjpeg-dev \
+    zlib1g-dev \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
 # Salin semua file dari folder lokal ke dalam container
-COPY . ./
+COPY . .
 
 # Install dependensi Python
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Menjalankan aplikasi Flask menggunakan Gunicorn
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
+CMD ["gunicorn", "--bind", "0.0.0.0:8080", "--workers", "1", "--threads", "8", "--timeout", "0", "main:app"]
--- main.py
+++ main.py
@@ -1,40 +1,63 @@
-from flask import Flask, request, jsonify
-from tensorflow.keras.preprocessing import image as keras_image
-from tensorflow.keras.applications.efficientnet import preprocess_input
-import tensorflow as tf
-from PIL import Image
 import io
+from flask import Flask, request, jsonify
+import tensorflow as tf
 import numpy as np
+from PIL import Image
+
+class_names1 = ["Padi", "Bukan Padi"]
+
+class_names2 = ["Bacterial Leaf Blight", "Blast", "BrownSpot", "Tungro"]
+
+disease_info = {
+    "Bacterial Leaf Blight": {
+        "penyebab": "Penyakit hawar daun bakteri (HDB) disebabkan oleh Xanthomonas oryzae pv. Oryzae.",
+        "ciri_ciri": "Munculnya bercak atau lesi berbentuk garis memanjang sejajar dengan tulang daun berwarna kuning yang kemudian berubah menjadi cokelat pada daun.",
+        "gejala": "Muncul garis-garis berwarna hijau muda hingga hijau keabu-abuan yang berair pada daun. Seiring waktu garis-garis tersebut dapat menyatu membentuk luka yang lebih besar dengan tepian yang tidak rata. Semakin lama daun akan menguning, kemudian layu, dan akhirnya mati.",
+        "rekomendasi_obat": "Gunakan fungisida berbasis tembaga seperti tembaga hidroksida atau oksiklorida.",
+        "perawatan": "Hentikan sementara pemberian pupuk nitrogen hingga penyakit terkendali, karena nitrogen berlebih dapat memperparah infeksi."
+    },
+    "Blast": {
+        "penyebab": "Penyakit blast disebabkan oleh jamur Pyricularia oryzae.",
+        "ciri_ciri": "Bercak berbentuk berlian dengan pusat abu-abu hingga putih dan tepi cokelat gelap.",
+        "gejala": "Daun berlubang, pelepah padi menguning, dan dapat menyebabkan patahnya batang jika parah.",
+        "rekomendasi_obat": "Gunakan fungisida berbasis triazol seperti propikonazol atau tebukonazol.",
+        "perawatan": "Potong dan buang daun padi yang terinfeksi parah, terutama yang sudah menunjukkan gejala bercak besar. Hal ini untuk mengurangi sumber inokulum (jamur) yang dapat menyebar ke tanaman lain."
+    },
+    "BrownSpot": {
+        "penyebab": "Penyakit Brownspot (bercak coklat) disebabkan oleh jamur Cercospora janseane (Racib) O. Const.",
+        "ciri_ciri": "Bercak cokelat bundar kecil pada daun, yang berkembang menjadi bercak lebih besar dengan pusat abu-abu.",
+        "gejala": "Daun mengering, hasil panen menurun, dan pada kasus berat dapat menyebabkan kematian tanaman muda.",
+        "rekomendasi_obat": "Gunakan fungisida berbasis mankozeb atau klorotalonil.",
+        "perawatan": "Pastikan keseimbangan nutrisi dengan pupuk kaya nitrogen dan kalium, serta hindari stres air pada tanaman."
+    },
+    "Tungro": {
+        "penyebab": "Penyebab penyakit tungro pada tanaman padi adalah infeksi dari dua jenis virus yaitu Rice Tungro Spherical Virus (RTSV) dan Rice Tungro Bacilliform Virus (RTBV). Virus ini ditularkan oleh serangga yang dianggap bahaya sekaligus hama bagi tanaman padi, yaitu wereng hijau.",
+        "ciri_ciri": "Daun berubah kuning kemerahan, pertumbuhan terhambat, dan anakan tanaman berkurang.",
+        "gejala": "Gejala utama penyakit tungro terlihat pada perubahan warna daun terutama pada daun muda berwarna kuning oranye dimulai dari ujung daun. Daun muda agak menggulung, jumlah anakan berkurang, tanaman kerdil dan pertumbuhan terhambat.",
+        "rekomendasi_obat": "Kendalikan vektor dengan insektisida seperti imidakloprid atau tiametoksam.",
+        "perawatan": "Gunakan varietas tahan tungro, kendalikan populasi wereng dengan insektisida, dan hindari tumpang sari tanaman padi yang sakit."
+    }
+}
 
 app = Flask(__name__)
 
-interpreter = tf.lite.Interpreter(model_path='converted_model_RIPAD_PART3.tflite')  
-interpreter.allocate_tensors()
+def load_tflite_model(model_path):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
 
-labels = ['Brown Spot', 'Bacterial Leaf Blight', 'Normal', 'Blast', 'Tungro']
+def predict_with_tflite(interpreter, input_data):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data
 
-disease_info = {
-    "Brown Spot": {
-      "gejala": "Bercak-bercak coklat pada daun dengan tepian yang kabur, sering kali menyebabkan penurunan produktivitas jika tidak segera ditangani.",
-      "pengobatan": "Gunakan fungisida yang mengandung Mancozeb atau Propiconazole secara teratur, hindari kelembapan berlebih, dan pastikan rotasi tanaman dilakukan."
-    },
-    "Bacterial Leaf Blight": {
-      "gejala": "Daun menunjukkan bercak air dengan batas yang jelas, seringkali dimulai dari ujung daun dan dapat menyebar cepat terutama di kondisi lembap.",
-      "pengobatan": "Gunakan antibiotik berbasis tembaga sesuai petunjuk, perbaiki sistem drainase untuk mengurangi kelembapan, dan gunakan benih yang tahan penyakit."
-    },
-    "Normal": {
-      "gejala": "Tidak ada gejala penyakit, tanaman dalam kondisi sehat dengan daun hijau dan produktivitas optimal.",
-      "pengobatan": "Tidak diperlukan pengobatan, namun disarankan untuk tetap menjaga kesehatan tanaman melalui pemupukan rutin dan pengawasan hama secara berkala."
-    },
-    "Blast": {
-      "gejala": "Bercak coklat dengan titik hitam kecil di tengahnya, seringkali muncul di daun dan batang. Serangan parah dapat menyebabkan kerontokan daun.",
-      "pengobatan": "Gunakan fungisida berbahan aktif Tricyclazole atau Isoprothiolane segera setelah gejala muncul, dan pastikan penggunaan pupuk nitrogen tidak berlebihan."
-    },
-    "Tungro": {
-      "gejala": "Daun menguning dengan pola yang tidak merata, tanaman kerdil, dan pertumbuhannya terhambat. Tanaman bisa mati jika serangan sangat parah.",
-      "pengobatan": "Gunakan insektisida untuk mengendalikan vektor penyakit ini (nyamuk hijau) dan tanam varietas padi yang tahan tungro untuk mencegah penyebaran."
-    }
-  }
+model_non_paddy_tflite = load_tflite_model('converted_model_paddyvsnonpaddy.tflite')
+model_paddy_disease_tflite = load_tflite_model('converted_model_paddy_Valent.tflite')
 
 @app.route('/')
 def home():
@@ -51,40 +74,42 @@ def predict():
         return jsonify({'error': 'No image selected'}), 400
 
     try:
-        img = Image.open(io.BytesIO(image_file.read()))
+        image = Image.open(image_file.stream)
 
-        if img.mode != 'RGB':
-            img = img.convert('RGB')
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
 
-        img = img.resize((224, 224))
-        x = keras_image.img_to_array(img)
+        img = image.resize((224, 224))
+
+        x = np.array(img)
         x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
+        x = x / 255.0
+        x = x.astype(np.float32)
 
-        input_details = interpreter.get_input_details()
-        interpreter.set_tensor(input_details[0]['index'], x)
+        predictions = predict_with_tflite(model_non_paddy_tflite, x)
+        predicted_probability_non_paddy = predictions[0][0]  
+        predicted_class_non_paddy = class_names1[1] if predicted_probability_non_paddy > 0.5 else class_names1[0]
 
-        interpreter.invoke()
+        if predicted_class_non_paddy == "Padi":
+            predictions = predict_with_tflite(model_paddy_disease_tflite, x)
+            predicted_class_index_paddy = np.argmax(predictions[0])
+            predicted_class_paddy = class_names2[predicted_class_index_paddy]
 
-        output_details = interpreter.get_output_details()
-        proba = interpreter.get_tensor(output_details[0]['index'])[0]
-
-        max_index = np.argmax(proba)
-        max_label = labels[max_index]
-        max_proba = proba[max_index]
-
-        result = {
-            'label': max_label,
-            'probability': float(max_proba * 100),
-            'gejala': disease_info[max_label]['gejala'],
-            'pengobatan': disease_info[max_label]['pengobatan']
-        }
+            result = {
+                'label': predicted_class_paddy,
+                'penyebab': disease_info[predicted_class_paddy]["penyebab"],
+                'ciri_ciri': disease_info[predicted_class_paddy]["ciri_ciri"],
+                'gejala': disease_info[predicted_class_paddy]["gejala"],
+                'rekomendasi_obat': disease_info[predicted_class_paddy]["rekomendasi_obat"],
+                'perawatan': disease_info[predicted_class_paddy]["perawatan"]
+            }
+        else:
+            result = {'message': "This is not a Padi, so no disease prediction needed."}
 
         return jsonify(result)
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
